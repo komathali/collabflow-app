63174ce397f25247686f077dece3957de21bdb33
K Ali Shahad <shahadali7sha@gmail.com>
2025-12-28 17:59:13 +0000

    Error: ENOENT: no such file or directory, lstat '/vercel/path0/.next/ser

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,24 +2,21 @@ import React from 'react';
 import { SidebarProvider, Sidebar, SidebarInset } from '@/components/ui/sidebar';
 import { SidebarNav } from '@/components/layout/sidebar-nav';
 import { Header } from '@/components/layout/header';
-import ProtectedRoute from '@/components/auth/protected-route';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ProtectedRoute>
-      <SidebarProvider>
-        <div className="flex min-h-screen">
-          <SidebarNav />
-          <div className="flex flex-col w-full">
-            <Header />
-            <SidebarInset>
-              <main className="flex-1 p-4 md:p-6 lg:p-8">
-                {children}
-              </main>
-            </SidebarInset>
-          </div>
+    <SidebarProvider>
+      <div className="flex min-h-screen">
+        <SidebarNav />
+        <div className="flex flex-col w-full">
+          <Header />
+          <SidebarInset>
+            <main className="flex-1 p-4 md:p-6 lg:p-8">
+              {children}
+            </main>
+          </SidebarInset>
         </div>
-      </SidebarProvider>
-    </ProtectedRoute>
+      </div>
+    </SidebarProvider>
   );
 }
--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { FirebaseClientProvider } from "@/firebase";
+import ProtectedRoute from "@/components/auth/protected-route";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
     return (
         <FirebaseClientProvider>
-            {children}
+            <ProtectedRoute>
+                {children}
+            </ProtectedRoute>
         </FirebaseClientProvider>
     )
 }
